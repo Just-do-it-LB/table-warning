6205c9210c4f4e6f06056e035f2206206a68fe54
Just-do-it-LB <60215541+Just-do-it-LB@users.noreply.github.com>
2023-01-04 09:47:46 +0800

    1 file modified, 1 added

---
+++ src/css1_opacity.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>The opacity property</h1>
+    <div id="myDIV">
+      <div id="div2">A yellow DIV element.</div>
+      <div id="div1">Change the opacity of the blue DIV.</div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+body {
+  background-color: #e7e9eb;
+}
+#myDIV {
+  height: 300px;
+  background-color: #ff0f0f;
+}
+#div2 {
+  border: 1px solid black;
+  background-color: yellow;
+  width: 200px;
+  height: 100px;
+  position: relative;
+  top: 80px;
+  left: 20px;
+  padding: 20px;
+}
+#div1 {
+  border: 1px solid black;
+  position: relative;
+  left: 50px;
+  padding: 20px;
+  background-color: lightblue;
+  width: 200px;
+  height: 100px;
+  opacity: 0.4;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
+import Vue from 'vue';
+import TDesign from 'tdesign-vue';
 
-  import Vue from 'vue';
-  import TDesign from 'tdesign-vue';
+// 引入组件库全局样式资源
+import 'tdesign-vue/es/style/index.css';
+import './index.css';
+// import Demo from './demo.vue';
+import opacity from './css1_opacity.vue';
 
-  // 引入组件库全局样式资源
-  import 'tdesign-vue/es/style/index.css';
-  import './index.css';
-  import Demo from './demo.vue';
+Vue.use(TDesign);
+Vue.config.productionTip = false;
 
-  Vue.use(TDesign);
-  Vue.config.productionTip = false;
-
-  new Vue({
-    render: (h) => h(Demo),
-  }).$mount('#app');
+new Vue({
+  render: (h) => h(opacity),
+}).$mount('#app');
